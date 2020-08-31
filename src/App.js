cc9dbb5b7929a44f15b6e612c83b2470333a7111
residentevil0803 <ramseyklassen0803@gmail.com>
2020-08-31 20:50:43 +0200

    initial base project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      app is working now <button>submit</button>
+    </div>
+  );
+}
