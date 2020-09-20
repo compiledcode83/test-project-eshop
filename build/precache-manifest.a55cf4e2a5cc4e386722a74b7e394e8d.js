37e8d29637d734bd3b8857774a96cab463e3e97a
residentevil0803 <ramseyklassen0803@gmail.com>
2020-09-20 08:05:45 +0200

    add production build foder to prepare for lunching

---
+++ build/precache-manifest.a55cf4e2a5cc4e386722a74b7e394e8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ff99ace127837b40e91409f5f41069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a4dfbd195dd771ba106",
+    "url": "/static/css/main.f28eca17.chunk.css"
+  },
+  {
+    "revision": "cfe4e44083042133da44",
+    "url": "/static/js/2.bed17fba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.bed17fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4dfbd195dd771ba106",
+    "url": "/static/js/main.b824a838.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "03789eff841f7793f040cfcbda3de8ac",
+    "url": "/static/media/main.03789eff.jpg"
+  },
+  {
+    "revision": "31ceb503469f8069eb3a6ace71e3bb03",
+    "url": "/static/media/main.31ceb503.jpg"
+  },
+  {
+    "revision": "99fb9350fc6f5ac75645fd83087f74e4",
+    "url": "/static/media/main.99fb9350.jpg"
+  },
+  {
+    "revision": "c4cdd2be0b75fd54d3323992e7bb5f7e",
+    "url": "/static/media/main.c4cdd2be.jpg"
+  }
+]);
